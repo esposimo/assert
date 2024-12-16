a4c2725e2fb6247cb5a7ea64c35729a9c25d61bd
smnDev <simone.esposito1986@gmail.com>
2024-12-16 23:58:57 +0100

    AbstractAssertion class

--- src/Equals.php
+++ src/Equals.php
@@ -5,6 +5,35 @@ namespace esposimo\assert;
 class Equals extends AbstractAssertion
 {
 
-    protected string $value = 'simo';
+    /**
+     * Configura se il test deve essere eseguito in maniera case-sensitive.
+     *
+     * @var bool true se il confronto distingue tra maiuscole e minuscole, false altrimenti.
+     */
+    protected bool $cs = false;
 
+
+    public function test(): bool
+    {
+        if (gettype($this->leftOperand) != gettype($this->rightOperand))
+        {
+            return false;
+        }
+
+        if (is_string($this->leftOperand))
+        {
+            if ($this->cs)
+            {
+                return (strcmp($this->leftOperand, $this->rightOperand) === 0);
+            }
+            return (strcasecmp($this->leftOperand, $this->rightOperand) === 0);
+        }
+
+        if (is_numeric($this->leftOperand))
+        {
+            return ($this->leftOperand == $this->rightOperand);
+        }
+
+        return false;
+    }
 }
